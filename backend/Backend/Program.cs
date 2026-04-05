ff4361575504e1f506a23204aca29089ab8b3492
Isaac Delph <isaac@Isaacs-Mac-mini.local>
2026-04-04 18:37:32 -0700

    Updated gitignore

--- backend/Backend/Program.cs
+++ backend/Backend/Program.cs
@@ -4,9 +4,14 @@ using Backend.Models;
 var builder = WebApplication.CreateBuilder(args);
 // Services
 builder.Services.AddControllers();
+
 string usersDbConn = builder.Configuration.GetConnectionString("UsersDb")
     ?? throw new ArgumentNullException("[Users Database Connection String] string is null");
 builder.Services.AddDbContext<UsersDbContext>(op => op.UseSqlite(usersDbConn));
+
+string listingsDbConn = builder.Configuration.GetConnectionString("ListingsDb")
+    ?? throw new ArgumentNullException("[Listings Database Connection String] string is null");
+builder.Services.AddDbContext<ListingsDbContext>(op => op.UseSqlite(listingsDbConn));
 
 var app = builder.Build();
 
--- backend/Models/LoginModel.cs
+++
@@ -1,6 +0,0 @@
-using backend.Models;
-
-public class LoginModel
-{
-    
-}
